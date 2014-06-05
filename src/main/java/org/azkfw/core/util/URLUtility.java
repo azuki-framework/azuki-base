43bb51faa6d32a9fe0146cc72bdf4cb29c597fea
Nobuyoshi Kawakita <nobuyoshi.kawakita@kdl.co.jp>
2014-06-05 17:27:24 +0900

    Add URLUtility class.

---
+++ src/main/java/org/azkfw/core/util/URLUtility.java
@@ -0,0 +1,107 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.azkfw.core.util;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * このクラスは、URL操作を行うユーティリティクラスです。
+ * 
+ * @since 1.0.1
+ * @version 1.0.1 2014/06/05
+ * @author Kawakicchi
+ */
+public class URLUtility {
+
+	/**
+	 * コンストラクタ
+	 * <p>
+	 * インスタンス生成を禁止する。
+	 * </p>
+	 */
+	private URLUtility() {
+
+	}
+
+	public static String get(final String aBaseURL, final String aTargetURL) throws MalformedURLException {
+		URL base = new URL(aBaseURL);
+		URL url = get(base, aTargetURL);
+		if (null != url) {
+			return url.toExternalForm();
+		} else {
+			return null;
+		}
+	}
+
+	public static URL get(final URL aBaseURL, final String aTargetURL) throws MalformedURLException {
+		URL url = null;
+
+		String host = aBaseURL.getProtocol() + "://" + aBaseURL.getHost() + ((-1 != aBaseURL.getPort()) ? ":" + aBaseURL.getPort() : "");
+		String file = aBaseURL.getFile();
+		String path = aBaseURL.getPath();
+		String query = aBaseURL.getQuery();
+
+		if (-1 != aTargetURL.indexOf("./")) { // 
+			// TODO: 未対応		
+
+		} else if (-1 != aTargetURL.indexOf("://")) { // 別ホストへ移動
+			url = new URL(aTargetURL);
+
+		} else if (0 == aTargetURL.length()) { // 同じ場所 -> #があればどける
+			url = new URL(host + file);
+
+		} else if (aTargetURL.startsWith("#")) { // 表示位置移動 -> #があれば切り替え
+			if (-1 == path.indexOf(".")) { // directory
+				if (file.endsWith("/") || StringUtility.isNotEmpty(query)) {
+					url = new URL(host + file + aTargetURL);
+				} else {
+					url = new URL(host + file + "/" + aTargetURL);
+				}
+			} else { // file
+				url = new URL(host + file + aTargetURL);
+			}
+
+		} else if (aTargetURL.startsWith("/")) { // ホストまで戻る
+			url = new URL(host + aTargetURL);
+
+		} else {
+			if (0 == file.length()) {
+				url = new URL(host + "/" + aTargetURL);
+
+			} else if (file.endsWith("/")) {
+				url = new URL(host + file + aTargetURL);
+
+			} else if (-1 == path.indexOf(".")) {
+				url = new URL(host + path + "/" + aTargetURL);
+
+			} else {
+				int index = path.lastIndexOf("/");
+				if (-1 == index) {
+					url = new URL(host + "/" + aTargetURL);
+
+				} else {
+					url = new URL(host + path.substring(0, index + 1) + aTargetURL);
+
+				}
+			}
+		}
+
+		return url;
+	}
+}
---
+++ src/test/java/org/azkfw/core/util/URLUtilityTest.java
@@ -0,0 +1,82 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.azkfw.core.util;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class URLUtilityTest extends TestCase {
+
+	@Test
+	public void testA() {
+
+		try {
+			assertEquals("https://127.0.0.1", URLUtility.get("http://localhost", "https://127.0.0.1"));
+			assertEquals("https://127.0.0.1/", URLUtility.get("http://localhost", "https://127.0.0.1/"));
+			assertEquals("https://127.0.0.1/test.do", URLUtility.get("http://localhost", "https://127.0.0.1/test.do"));
+			assertEquals("https://127.0.0.1/test.do?aaa=BBB", URLUtility.get("http://localhost", "https://127.0.0.1/test.do?aaa=BBB"));
+			assertEquals("https://127.0.0.1/test.do#top", URLUtility.get("http://localhost", "https://127.0.0.1/test.do#top"));
+
+			assertEquals("http://localhost/test.do", URLUtility.get("http://localhost", "/test.do"));
+			assertEquals("http://localhost/test.do", URLUtility.get("http://localhost/", "/test.do"));
+			assertEquals("http://localhost/test.do", URLUtility.get("http://localhost/index.do", "/test.do"));
+			assertEquals("http://localhost/test.do", URLUtility.get("http://localhost/aaaa", "/test.do"));
+			assertEquals("http://localhost/test.do", URLUtility.get("http://localhost/aaaa/", "/test.do"));
+			assertEquals("http://localhost/test.do", URLUtility.get("http://localhost/aaaa/index.do", "/test.do"));
+			assertEquals("http://localhost/test.do?aaa=BBB", URLUtility.get("http://localhost/aaaa/index.do", "/test.do?aaa=BBB"));
+			assertEquals("http://localhost/test.do#bottom", URLUtility.get("http://localhost/aaaa/index.do", "/test.do#bottom"));
+
+			assertEquals("http://localhost", URLUtility.get("http://localhost", ""));
+			assertEquals("http://localhost/", URLUtility.get("http://localhost/", ""));
+			assertEquals("http://localhost/index.do", URLUtility.get("http://localhost/index.do", ""));
+			assertEquals("http://localhost/aaaa", URLUtility.get("http://localhost/aaaa", ""));
+			assertEquals("http://localhost/aaaa/", URLUtility.get("http://localhost/aaaa/", ""));
+			assertEquals("http://localhost/aaaa/index.do", URLUtility.get("http://localhost/aaaa/index.do", ""));
+			assertEquals("http://localhost/aaaa/index.do?aaa=bbb", URLUtility.get("http://localhost/aaaa/index.do?aaa=bbb", ""));
+			assertEquals("http://localhost/aaaa/index.do", URLUtility.get("http://localhost/aaaa/index.do#top", ""));
+
+			assertEquals("http://localhost/#bottom", URLUtility.get("http://localhost", "#bottom"));
+			assertEquals("http://localhost/#bottom", URLUtility.get("http://localhost/", "#bottom"));
+			assertEquals("http://localhost/index.do#bottom", URLUtility.get("http://localhost/index.do", "#bottom"));
+			assertEquals("http://localhost/aaaa/#bottom", URLUtility.get("http://localhost/aaaa", "#bottom"));
+			assertEquals("http://localhost/aaaa?aaa=bbb#bottom", URLUtility.get("http://localhost/aaaa?aaa=bbb", "#bottom"));
+			assertEquals("http://localhost/aaaa/#bottom", URLUtility.get("http://localhost/aaaa/", "#bottom"));
+			assertEquals("http://localhost/aaaa/index.do#bottom", URLUtility.get("http://localhost/aaaa/index.do", "#bottom"));
+			assertEquals("http://localhost/aaaa/index.do?aaa=bbb#bottom", URLUtility.get("http://localhost/aaaa/index.do?aaa=bbb", "#bottom"));
+			assertEquals("http://localhost/aaaa/index.do#bottom", URLUtility.get("http://localhost/aaaa/index.do#top", "#bottom"));
+
+			assertEquals("http://localhost/test.do", URLUtility.get("http://localhost", "test.do"));
+			assertEquals("http://localhost/test.do", URLUtility.get("http://localhost/", "test.do"));
+			assertEquals("http://localhost/test.do", URLUtility.get("http://localhost/index.do", "test.do"));
+			assertEquals("http://localhost/aaaa/test.do", URLUtility.get("http://localhost/aaaa", "test.do"));
+			assertEquals("http://localhost/aaaa/test.do", URLUtility.get("http://localhost/aaaa/", "test.do"));
+			assertEquals("http://localhost/aaaa/test.do", URLUtility.get("http://localhost/aaaa/index.do", "test.do"));
+			assertEquals("http://localhost/aaaa/test.do", URLUtility.get("http://localhost/aaaa/index.do?aaa=bbb", "test.do"));
+			assertEquals("http://localhost/aaaa/test.do", URLUtility.get("http://localhost/aaaa/index.do#top", "test.do"));
+
+			assertNull(URLUtility.get("http://localhost", "./"));
+			assertNull(URLUtility.get("http://localhost", "../"));
+			assertNull(URLUtility.get("http://localhost", "./aaa"));
+			assertNull(URLUtility.get("http://localhost", "../bbb"));
+
+		} catch (Exception ex) {
+			fail();
+		}
+	}
+}
